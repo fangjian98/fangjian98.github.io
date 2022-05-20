832fa1b86fd3e0167e0cba795a171d0e8f045d37
fangjian98 <2360870585@qq.com>
2022-05-20 11:50:38 +0800

    update shell

--- auto_generate_hexo_and_push_github.sh
+++ auto_generate_hexo_and_push_github.sh
@@ -7,9 +7,9 @@ echo "hexo generate and deploy end"
 echo "git push remote repo start"
 git add .
 if [ $# -lt 1 ];then
-	git commit -m "add new blog"	
+    git commit -m "add new blog"
 else
-	git commit -m "$1"
+    git commit -m "$1"
 fi
 git push
 echo "git push remote repo end"
